ca0d8ee3c685f369f6d9a05c2c8cf2a2d26f238a
RG2021 <47146889+RG2021@users.noreply.github.com>
2019-07-14 13:39:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Importing Libraries.
 import json
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 
+# Reading the training file.
 i=0
 train=[]
 topic=[]
@@ -17,24 +19,40 @@ with open('training.json','r') as f:
         topic.append(j["topic"])
         train.append(j["excerpt"])
 
-train=np.array(train)
+i=0
 test=[]
-N = int(input())
-for i in range(N):
-    j = json.loads(input())
-    test.append(j['excerpt'])
+test_output=[]
+
+with open('test_input.json') as f:
+    for line in f:
+        i=i+1;
+        if(i==1):
+            continue
+        j=json.loads(line)
+        test.append(j['excerpt'])
+
+with open('test_output.json') as f:
+    for line in f:
+        test_output.append(line)
+        
+train=np.array(train)
 test=np.array(test)
-    
+
 vect = TfidfVectorizer(sublinear_tf=True, analyzer = 'word', ngram_range=(1,1), stop_words='english', max_features=None)
-X = vect.fit_transform(train)
+x_train = vect.fit_transform(train)
+x_test = vect.transform(test)
+
 le=preprocessing.LabelEncoder()
-Y=le.fit_transform(topic)
-X_test = vect.transform(test)
+y_train = le.fit_transform(topic)
 
 clf = MultinomialNB()
-clf.fit(X, Y)
+clf.fit(x_train, y_train)
+y_predict = clf.predict(x_test)
+y_predict=le.inverse_transform(y_predict)
 
-predictions = clf.predict(X_test)
-for i in predictions:
-    a=le.inverse_transform([i])
-    print(a[0])
+test_output=np.array(test_output)
+y_test=[]
+for i in test_output:
+    y_test.append(i.rstrip())
+accuracy=accuracy_score(y_test, y_predict)
+print(accuracy)
